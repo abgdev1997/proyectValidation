cd8e963c75a7afd7f2c9e98a4df9c64558cdb941
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-22 12:47:33 +0100

    --

---
+++ src/main/java/com/proyectValidation/proyectValidation/ProyectValidationApplication.java
@@ -0,0 +1,13 @@
+package com.proyectValidation.proyectValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proyectValidation/proyectValidation/ProyectValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectValidation.proyectValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
