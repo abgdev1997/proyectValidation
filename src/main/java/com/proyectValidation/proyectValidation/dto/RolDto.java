cd0cf31cafe309f28ae695a7137a37131296c31a
Josema <josema.saborido@gmail.com>
2021-11-26 20:13:05 +0100

    Creado RolDto y añadido atributos de rol de usuario en User

---
+++ src/main/java/com/proyectValidation/proyectValidation/dto/RolDto.java
@@ -0,0 +1,10 @@
+package com.proyectValidation.proyectValidation.dto;
+
+/**4
+ * Enum de Rol. Especifica el rol del usuario para asignarle privilegios.
+ * @author Josema
+ * @version 1.0
+ */
+public enum RolDto {
+    USER, ADMIN
+}
