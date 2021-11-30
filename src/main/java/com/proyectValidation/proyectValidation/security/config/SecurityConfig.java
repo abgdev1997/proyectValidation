9684dec5cc054b9221b5a83bde45ed5233f09090
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 12:58:24 +0100

    permiso http://localhost:3000

--- src/main/java/com/proyectValidation/proyectValidation/security/config/SecurityConfig.java
+++ src/main/java/com/proyectValidation/proyectValidation/security/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource()
     {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("localhost:3000"));
+        configuration.setAllowedOriginPatterns(List.of("http://localhost:3000"));
         configuration.setAllowedMethods(List.of("GET", "POST", "DELETE", "PUT"));
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
         configuration.setAllowCredentials(true);
