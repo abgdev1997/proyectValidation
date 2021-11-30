b4e2638323dc78f0891d0f9085669cadcdc893f3
abgdev1997 <adrianblancogarcia97@gmail.com>
2021-11-30 13:13:16 +0100

    cross origin qr

--- src/main/java/com/proyectValidation/proyectValidation/controller/QRCodeController.java
+++ src/main/java/com/proyectValidation/proyectValidation/controller/QRCodeController.java
@@ -3,11 +3,9 @@ package com.proyectValidation.proyectValidation.controller;
 import com.proyectValidation.proyectValidation.service.QRCodeGenerator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/QR")
 public class QRCodeController {
