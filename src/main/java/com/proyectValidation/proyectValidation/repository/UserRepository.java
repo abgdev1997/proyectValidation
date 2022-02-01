befab079c099a6abce60cd98e6ef2e114367d1be
abgdev1997 <adrianblancogarcia97@gmail.com>
2022-02-01 09:43:43 +0100

    done

--- src/main/java/com/proyectValidation/proyectValidation/repository/UserRepository.java
+++ src/main/java/com/proyectValidation/proyectValidation/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.proyectValidation.proyectValidation.repository;
 
 import com.proyectValidation.proyectValidation.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
     Optional<User> findByUserName(String userName);
